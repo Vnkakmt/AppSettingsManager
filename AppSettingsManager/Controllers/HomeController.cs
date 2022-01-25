ffb44efedae687fe38e89e523f923b30245c7772
Vinayak <vnkakmt@gmail.com>
2022-01-25 11:48:46 +0530

    Different ways of Retrieving values from appSettings in controller and view

--- AppSettingsManager/Controllers/HomeController.cs
+++ AppSettingsManager/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using AppSettingsManager.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -12,14 +13,25 @@ namespace AppSettingsManager.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _config;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration config)
         {
             _logger = logger;
+            _config = config;
         }
 
         public IActionResult Index()
         {
+            ViewBag.SendGridKey = _config.GetValue<string>("SendGridKey");
+            //ViewBag.TwilioAuthToken = _config.GetValue<string>("Twilio:AuthToken");
+            ViewBag.TwilioAuthToken = _config.GetSection("Twilio").GetValue<string>("AuthToken");
+            ViewBag.TwilioAuthSid = _config.GetValue<string>("Twilio:AccountSid");
+
+            //ViewBag.ThirdLevelsetting = _config.GetValue<string>("FirstLevelSetting:SecondLevelSetting:BottomLevelSetting");
+            //ViewBag.ThirdLevelsetting = _config.GetSection("FirstLevelSetting").GetValue<string>("SecondLevelSetting:BottomLevelSetting");
+            //ViewBag.ThirdLevelsetting = _config.GetSection("FirstLevelSetting").GetSection("SecondLevelSetting").GetValue<string>("BottomLevelSetting");
+            ViewBag.ThirdLevelsetting = _config.GetSection("FirstLevelSetting").GetSection("SecondLevelSetting").GetSection("BottomLevelSetting").Value;
             return View();
         }
 
